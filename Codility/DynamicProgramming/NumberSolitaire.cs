f6f72d2084279a523a3546a62bfd00c5b46ec640
Nicu Maxian <maxiannicu@gmail.com>
2018-07-10 23:28:36 +0300

    Refactoring

---
+++ Codility/DynamicProgramming/NumberSolitaire.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Codility
+{
+    public class NumberSolitaire
+    {
+        public int solution(int[] A)
+        {
+            var pastResults = new int[A.Length];
+
+            pastResults[0] = A[0];
+
+            for (int i = 1; i < A.Length; i++)
+            {
+                pastResults[i] = pastResults[i - 1] + A[i];
+                for (int j = 2; j <= 6 && i - j >= 0; j++)
+                {
+                    pastResults[i] = Math.Max(pastResults[i - j] + A[i], pastResults[i]);
+                }
+            }
+
+            return pastResults[A.Length - 1];
+        }
+    }
+}
--- Codility/Program.cs
+++ Codility/Program.cs
@@ -1,15 +1,76 @@
 ﻿using System;
+using System.Linq;
 
 namespace Codility
 {
     class Program
     {
+        public int solution(int[] A)
+        {
+            if (A.Length == 0)
+            {
+                return 0;
+            }
+
+            var max = A[0];
+            var sum = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = Math.Abs(A[i]);
+                max = Math.Max(A[i], max);
+                sum += A[i];
+            }
+
+            var count = new int[max + 1];
+            var dp = new int[sum + 1];
+
+            for (int i = 1; i < dp.Length; i++)
+            {
+                dp[i] = -1;
+            }
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                count[A[i]]++;
+            }
+
+            for (int i = 1; i <= max; i++)
+            {
+                if (count[i] > 0)
+                {
+                    for (int j = 0; j < sum; j++)
+                    {
+                        if (dp[j] >= 0)
+                        {
+                            dp[j] = count[i];
+                        }
+                        else if (j >= i && dp[j - i] > 0)
+                        {
+                            dp[j] = dp[j - i] - 1;
+                        }
+                    }
+                }
+            }
+
+            var result = sum;
+            for (int i = 0; i < sum / 2 + 1; i++)
+            {
+                if (dp[i] >= 0)
+                {
+                    result = Math.Min(result, sum - 2 * i);
+                }
+            }
+
+            return result;
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(new Program().solution(4, new[]
+            Console.WriteLine(new Program().solution(new[]
             {
-                1, 2, 3, 4, 1, 1, 3
+                //3, 3, 3, 4, 5
+                9,8,7
             }));
         }
     }
